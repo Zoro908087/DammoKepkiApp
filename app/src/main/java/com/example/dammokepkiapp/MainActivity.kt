6aa0b1c7edc9a4f4418f5695e8177e4f44bdd854
luffi3000000000 <110896715+luffi3000000000@users.noreply.github.com>
2022-09-21 16:37:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dammokepkiapp/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.dammokepkiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.resources.Compatibility.Api21Impl.inflate
+import androidx.core.graphics.drawable.DrawableCompat.inflate
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        }
+}
